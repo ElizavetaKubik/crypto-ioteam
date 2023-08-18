5717d81efaca333ca8f77bda6b31fa6109677993
ElizavetaKubik <kubikelizavetaofficial@gmail.com>
2023-08-18 22:08:17 +0300

    refactor: add app theme

--- lib/features/coin_list/widgets/coin_tile_recommended.dart
+++ lib/features/coin_list/widgets/coin_tile_recommended.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:crypto_ioteam/config/routes/router.dart';
+import 'package:crypto_ioteam/core/util.dart';
 import 'package:crypto_ioteam/repository/models/coin_model.dart';
 import 'package:flutter/material.dart';
 
@@ -10,71 +11,64 @@ class CoinTileRecommended extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double appHeight = MediaQuery.of(context).size.height;
-    double appWidth = MediaQuery.of(context).size.width;
+    double screenHeight = getScreenHeight(context);
+    double screenWidth = getScreenWidth(context);
+
+    final theme = Theme.of(context);
+
     return Padding(
       padding: EdgeInsets.symmetric(
-          horizontal: appWidth * 0.03, vertical: appHeight * 0.02),
+        vertical: screenHeight * 0.02,
+        horizontal: screenWidth * 0.03,
+      ),
       child: GestureDetector(
         onTap: () {
           AutoRouter.of(context).push(CoinRoute(coin: coin));
         },
         child: Container(
           padding: EdgeInsets.only(
-            left: appWidth * 0.03,
-            right: appWidth * 0.05,
-            top: appHeight * 0.02,
-            bottom: appHeight * 0.02,
+            top: screenHeight * 0.02,
+            bottom: screenHeight * 0.01,
+            left: screenWidth * 0.03,
+            right: screenWidth * 0.05,
           ),
           decoration: BoxDecoration(
             border: Border.all(color: Colors.grey),
             borderRadius: BorderRadius.circular(20),
           ),
-          height: appHeight * 0.03,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                height: appHeight * 0.035,
+                height: screenHeight * 0.035,
                 child: Image.network(
                   coin.image,
                 ),
               ),
-              SizedBox(height: appHeight * 0.02),
+              SizedBox(height: screenHeight * 0.008),
               Text(
                 coin.name,
-                style:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                style: theme.textTheme.labelLarge,
               ),
-              SizedBox(height: appHeight * 0.01),
+              SizedBox(height: screenHeight * 0.008),
               Row(
                 children: [
                   Text(
                     coin.priceChange24H.toString().contains('-')
                         ? "-\$${coin.priceChange24H.toStringAsFixed(2).toString().replaceAll('-', '')}"
                         : "\$${coin.priceChange24H.toStringAsFixed(2)}",
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal,
-                      // color: coin.marketCapChangePercentage24H > 0
-                      //     ? Colors.green
-                      //     : Colors.red,
-                      color: Colors.grey,
-                    ),
+                    style: theme.textTheme.labelSmall,
                   ),
-                  SizedBox(width: appWidth * 0.03),
+                  SizedBox(width: screenWidth * 0.03),
                   Text(
                     '${coin.marketCapChangePercentage24H.toStringAsFixed(2)}%',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal,
-                      color: coin.marketCapChangePercentage24H > 0
-                          ? Colors.green
-                          : Colors.red,
-                    ),
+                    style: theme.textTheme.labelSmall?.copyWith(
+                        color: coin.marketCapChangePercentage24H > 0
+                            ? Colors.green
+                            : Colors.red),
                   ),
                 ],
-              )
+              ),
             ],
           ),
         ),
